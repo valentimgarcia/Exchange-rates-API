28b8434d7c3df36404adfa8b76bf13a7198c572f
Valentim Garcia <vldg10sd@gmail.com>
2022-04-30 14:03:59 +0100

    start to understand the api and spring boot

---
+++ src/main/java/com/example/Exchange/Rates/API/ExchangeRatesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exchange.Rates.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRatesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Exchange/Rates/API/ExchangeRatesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exchange.Rates.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
